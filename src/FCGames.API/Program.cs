bb769180e28a94cff8637b1175a6168ea74cc25a
SauloSGananca <saulo.gananca@atlasinovacoes.com.br>
2025-05-25 21:57:13 -0300

    ajustes configuracoes

--- src/FCGames.API/Program.cs
+++ src/FCGames.API/Program.cs
@@ -1,10 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
